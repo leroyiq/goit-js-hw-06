60c941cd03f88b27fc0cf3a94d3414facfd5f574
leroyiq <leroyiq6@gmail.com>
2022-10-11 19:32:39 +0300

    task-02 complete

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listRef = document.querySelector("#ingredients");
+const liItem = [];
+ingredients.forEach((item) => {
+  const elemRef = document.createElement("li");
+  elemRef.classList.add("item");
+  elemRef.textContent = item;
+  liItem.push(elemRef);
+});
+
+listRef.append(...liItem);
