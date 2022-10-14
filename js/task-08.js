032b3de3e2616b171400f6a2aea7c61d6401e02f
leroyiq <leroyiq6@gmail.com>
2022-10-14 18:06:45 +0300

    task-08 complete

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener("submit", checkFormFn);
+
+function checkFormFn(event) {
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    alert("Warning !!! All data field required");
+  } else {
+    const dataRef = {
+      email: email.value,
+      password: password.value,
+    };
+    console.dir(dataRef);
+  }
+  event.preventDefault();
+  event.currentTarget.reset();
+}
