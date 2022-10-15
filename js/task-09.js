fd46a2d14a1a1dd5d5d2f3b30473c18b4cc1766f
Leroy <leroyiq6@gmail.com>
2022-10-15 18:01:14 +0300

    task-09 complete

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const btnRef = document.querySelector(".change-color");
+const bodyRef = document.querySelector("body");
+const spanRef = document.querySelector(".color");
+
+btnRef.addEventListener("click", changeColorFn);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+function changeColorFn() {
+  bodyRef.style.background = `${getRandomHexColor()}`;
+  spanRef.textContent = `${getRandomHexColor()}`;
 }
