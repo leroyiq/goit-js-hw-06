dfb9fd25fcd55278dbd897a3605c1434b95e5ba8
leroyiq <leroyiq6@gmail.com>
2022-10-16 13:36:59 +0300

    task-06 editFix

--- js/task-06.js
+++ js/task-06.js
@@ -2,16 +2,18 @@ const inputRef = document.querySelector("#validation-input");
 
 const inputFocusOff = (event) => {
   const strRef = event.currentTarget.value;
-  if (strRef.length !== 0) {
-    if (strRef.length <= inputRef.dataset.length) {
+
+  if (strRef.length === 0 || strRef.length < Number(inputRef.dataset.length)) {
+    inputRef.classList.remove("valid");
+    inputRef.classList.remove("invalid");
+  } else {
+    if (strRef.length === Number(inputRef.dataset.length)) {
       inputRef.classList.remove("invalid");
       inputRef.classList.add("valid");
     } else {
-      inputRef.classList.replace("valid", "invalid");
+      inputRef.classList.remove("valid");
+      inputRef.classList.add("invalid");
     }
-  } else {
-    inputRef.classList.remove("invalid");
-    inputRef.classList.remove("valid");
   }
 };
 
