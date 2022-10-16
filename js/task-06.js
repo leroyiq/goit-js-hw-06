822066f71d870dac263a07166d236e2306baba13
leroyiq <leroyiq6@gmail.com>
2022-10-16 15:57:47 +0300

    task-06 Fixed and task-10 complete

--- js/task-06.js
+++ js/task-06.js
@@ -3,17 +3,12 @@ const inputRef = document.querySelector("#validation-input");
 const inputFocusOff = (event) => {
   const strRef = event.currentTarget.value;
 
-  if (strRef.length === 0 || strRef.length < Number(inputRef.dataset.length)) {
-    inputRef.classList.remove("valid");
+  if (strRef.length === Number(inputRef.dataset.length)) {
     inputRef.classList.remove("invalid");
+    inputRef.classList.add("valid");
   } else {
-    if (strRef.length === Number(inputRef.dataset.length)) {
-      inputRef.classList.remove("invalid");
-      inputRef.classList.add("valid");
-    } else {
-      inputRef.classList.remove("valid");
-      inputRef.classList.add("invalid");
-    }
+    inputRef.classList.remove("valid");
+    inputRef.classList.add("invalid");
   }
 };
 
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,37 @@
+const inputRef = document.querySelector("input");
+const creatBtnRef = document.querySelector("[data-create]");
+const destroyBtnRef = document.querySelector("[data-destroy]");
+const conteinerRef = document.querySelector("#boxes");
+
+inputRef.addEventListener("input", getCurrentInputValue);
+creatBtnRef.addEventListener("click", createBoxes);
+
+function getCurrentInputValue(event) {
+  return event.currentTarget.value;
+}
+
+function createBoxes(amount) {
+  conteinerRef.innerHTML = "";
+  amount = Number(inputRef.value);
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement("div");
+    box.style.width = `${30 + 10 * i}px`;
+    box.style.height = `${30 + 10 * i}px`;
+    box.style.backgroundColor = `${getRandomHexColor()}`;
+
+    box.style.marginTop = "20px";
+    conteinerRef.appendChild(box);
+  }
+}
+
+destroyBtnRef.addEventListener("click", destroyAllBoxes);
+function destroyAllBoxes() {
+  conteinerRef.innerHTML = "";
+  inputRef.value = "";
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, "0")}`;
 }
